8cad7fc4779233569b4c635fc4a6592dcfbc4607
Yuki Higuchi <129672772+reiz0@users.noreply.github.com>
2025-04-22 03:50:51 -0700

    implement NavigationMenue

--- src/components/common/FooterMenu.tsx
+++ src/components/common/FooterMenu.tsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu";
+import Link from "next/link";
 
 const FooterMenu = () => {
   return (
-    <div>FooterMenu</div>
-  )
-}
+    <NavigationMenu>
+      <NavigationMenuList>
+        <NavigationMenuItem>
+          <Link href="/calculate" legacyBehavior passHref>
+            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+              Calculate
+            </NavigationMenuLink>
+          </Link>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <Link href="/" legacyBehavior passHref>
+            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+              Home
+            </NavigationMenuLink>
+          </Link>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <Link href="/calender" legacyBehavior passHref>
+            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+              Calender
+            </NavigationMenuLink>
+          </Link>
+        </NavigationMenuItem>
+      </NavigationMenuList>
+    </NavigationMenu>
+  );
+};
 
-export default FooterMenu
+export default FooterMenu;
