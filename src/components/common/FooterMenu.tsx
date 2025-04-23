b030a48c4d0f7f0c237b02053d196dea4bca9d08
Yuki Higuchi <129672772+reiz0@users.noreply.github.com>
2025-04-23 15:13:36 -0700

    implement footer menu styling

--- src/components/common/FooterMenu.tsx
+++ src/components/common/FooterMenu.tsx
@@ -4,54 +4,59 @@ import {
   NavigationMenu,
   NavigationMenuItem,
   NavigationMenuList,
-  navigationMenuTriggerStyle,
 } from "@/components/ui/navigation-menu";
 import clsx from "clsx";
+import { Calculator, CalendarDays, House } from "lucide-react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
-const navigationMenuItems = [
-  {
-    link: "/calculate",
-    description: "Calculate",
-  },
-  {
-    link: "/",
-    description: "Home",
-  },
-  {
-    link: "/calender",
-    description: "Calender",
-  },
-];
-
 const FooterMenu = () => {
   const requestPathname = usePathname();
-  console.log(requestPathname);
+  const iconStyle = "h-5 w-5 mb-1";
+
+  const navigationMenuItems = [
+    {
+      link: "/calculate",
+      description: "Calculate",
+      icon: <Calculator className={iconStyle} />,
+    },
+    {
+      link: "/",
+      description: "Home",
+      icon: <House className={iconStyle} />,
+    },
+    {
+      link: "/calender",
+      description: "Calender",
+      icon: <CalendarDays className={iconStyle} />,
+    },
+  ];
 
   return (
-    <NavigationMenu className="absolute bottom-0">
-      <NavigationMenuList className="w-screen">
-        {/* <div className=""> */}
-        {navigationMenuItems.map((item, index) => (
-          <NavigationMenuItem
-            key={index}
-            className="flex justify-center w-full"
-          >
-            <Link
-              href={item.link}
-              className={clsx(
-                "w-full",
-                navigationMenuTriggerStyle(),
-                item.link === requestPathname && "bg-green-50"
-              )}
+    <NavigationMenu className="absolute bottom-0 border-t border-zinc-900">
+      <div className="w-screen flex justify-center">
+        <NavigationMenuList className="max-w-md w-screen mx-auto">
+          {navigationMenuItems.map((item, index) => (
+            <NavigationMenuItem
+              key={index}
+              className="flex justify-center w-full"
             >
-              {item.description}
-            </Link>
-          </NavigationMenuItem>
-        ))}
-        {/* </div> */}
-      </NavigationMenuList>
+              <Link
+                href={item.link}
+                className={clsx(
+                  "w-full rounded-none text-emerald-600 active:bg-zinc-900  active:text-emerald-400 transition-all duration-300 text-center flex flex-col justify-center items-center p-1",
+                  item.link === requestPathname && "bg-zinc-800"
+                )}
+              >
+                {item.icon}
+                <p className="text-xs uppercase tracking-wider font-extrabold">
+                  {item.description}
+                </p>
+              </Link>
+            </NavigationMenuItem>
+          ))}
+        </NavigationMenuList>
+      </div>
     </NavigationMenu>
   );
 };
