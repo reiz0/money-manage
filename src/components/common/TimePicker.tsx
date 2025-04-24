a87e3038a17cd9c9c5850fc223b32cec0412efc4
Yuki Higuchi <129672772+reiz0@users.noreply.github.com>
2025-04-24 15:13:38 -0700

    edit input component for adjusting this app design style

--- src/components/common/TimePicker.tsx
+++ src/components/common/TimePicker.tsx
@@ -15,7 +15,7 @@ export const TimePicker = ({ label, id }: Props) => {
         <Input
           type="time"
           id={id}
-          className="bg-emerald-900/30 border-emerald-800/50 h-12 text-emerald-50 pl-10"
+          className="pl-10 h-12"
         />
         <div className="absolute left-0 top-0 bottom-0 w-8 flex items-center justify-center bg-emerald-800/30 border-r border-emerald-800/30 text-emerald-400 p-2">
           {id === "startTime" && <ClockArrowUp />}
