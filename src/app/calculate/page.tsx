c1a05f9bc219a674189a2c1add3b83a4e78f7759
Yuki Higuchi <129672772+reiz0@users.noreply.github.com>
2025-05-19 23:48:55 -0700

    make add card function

--- src/app/calculate/page.tsx
+++ src/app/calculate/page.tsx
@@ -1,9 +1,17 @@
-import { WorkTimeCard } from "@/components/common/WorkTimeCard";
+"use client"
+import { WorkTimeCard } from "@/app/calculate/_components/WorkTimeCard";
+import { Button } from "@/components/ui/button";
+import { useState } from "react";
 
 export default function CalculatePage() {
+  const [totalWorkingHour, setTotalWorkingHour] = useState<number>(0)
+  const [WorkTimeCardNum, setWorkTimeCardNum] = useState<number>(1)
   return (
     <div>
-      <WorkTimeCard />
+      {[...Array(WorkTimeCardNum)].map((_, i) => (<WorkTimeCard key={i} />))}
+
+      <Button onClick={() => setWorkTimeCardNum(WorkTimeCardNum + 1)}>+</Button>
+      
     </div>
   );
 }
